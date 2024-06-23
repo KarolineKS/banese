d0b881b27caeeb04195d62442b81982647a88f60
KarolineKS <kkohlsstangherlin@gmail.com>
2024-06-23 18:58:43 -0300

    :lipstick: [feat]: Estilização da página inicial

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
@@ -15,13 +15,16 @@ export default function Home() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:8000/users/login", { cpf, password });
+      const res = await axios.post('http://localhost:8000/users/login', {
+        cpf,
+        password,
+      });
       if (res.data.error) {
         console.log(res.data.error);
       } else {
         console.log(res.data);
         localStorage.setItem('userCpf', cpf);
-        router.push('/contract');
+        router.push('/contrato');
       }
     } catch (error) {
       console.error(error);
@@ -32,47 +35,56 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-no-repeat bg-center bg-cover" style={{ backgroundImage: "url('/background.jpg')" }}>
+    <div
+      className="min-h-screen flex items-center justify-center bg-no-repeat bg-center bg-cover"
+      style={{ backgroundImage: "url('/background.jpg')" }}
+    >
       <form
         onSubmit={handleSubmit}
-        className="bg-white bg-opacity-40 backdrop-filter backdrop-blur-lg p-10 rounded-lg max-w-lg w-full mx-4 shadow-lg"
+        className="backdrop-filter backdrop-blur-sm p-10 rounded-xl max-w-xl w-[23vw] mx-4 flex flex-col justify-center items-center gap-5"
+        style={{
+          boxShadow: "rgb(0, 0, 0) 0px 20px 30px -10px, rgb(0, 0, 0) 0px -20px 30px -10px",
+          height: "55vh"
+        }}
+        
       >
-        <h2 className="text-4xl font-bold mb-8 text-center bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
-          Login
+        <h2 className="text-3xl font-bold mb-8 text-center text-white">
+          Área do Cliente
         </h2>
-        <div className="mb-6">
-          <label className="block text-gray-700 mb-3 font-semibold text-lg">CPF:</label>
-          <div className="relative">
-            <span className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
-              <FaUser className="text-gray-400" />
-            </span>
-            <InputMask
-              mask="999.999.999-99"
-              value={cpf}
-              onChange={(e) => setCpf(e.target.value)}
-              className="w-full p-4 border border-gray-300 rounded pl-12 text-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
+        <div className="mb-6 w-full flex flex-col gap-8">
+            <div className="relative w-full">
+              <span className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
+                <FaUser className="text-white" />
+              </span>
+              <InputMask
+                placeholder="CPF"
+                mask="999.999.999-99"
+                value={cpf}
+                onChange={(e) => setCpf(e.target.value)}
+                className="w-full bg-transparent p-2 border border-gray-300 rounded pl-12 text-lg focus:outline-none text-white placeholder-white"
+                required
+              />
           </div>
-        </div>
-        <div className="mb-8">
-          <label className="block text-gray-700 mb-3 font-semibold text-lg">Senha:</label>
-          <div className="relative">
-            <span className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
-              <FaLock className="text-gray-400" />
-            </span>
-            <input
-              type="password"
-              className="w-full pl-12 p-4 border border-gray-300 rounded text-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
+            <div className="relative w-full">
+              <span className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none">
+                <FaLock className="text-white" />
+              </span>
+              <input
+                placeholder='Senha'
+                type="password"
+                className="w-full pl-12 bg-transparent p-2 border border-gray-300 rounded text-lg focus:outline-none text-white placeholder-white"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
         </div>
         <button
           type="submit"
-          className="w-full bg-gradient-to-r from-green-400 to-blue-500 text-white py-3 rounded hover:from-green-500 hover:to-blue-600 transition duration-200 font-semibold text-lg"
+          className="w-full p-2 text-white rounded-lg transition duration-200 font-semibold text-lg bg-[#00C2A7] hover:bg-[#00A591]"
+          style={{
+            boxShadow: "rgb(0, 0, 0) 0px 20px 30px -10px",
+          }}
         >
           Login
         </button>
