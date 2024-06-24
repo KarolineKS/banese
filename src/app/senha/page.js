e747d7a3182caf0281f090bd529ca2410339eb05
KarolineKS <kkohlsstangherlin@gmail.com>
2024-06-24 18:26:32 -0300

    :rocket: [feat]: Arrumando bug da api

--- src/app/senha/page.js
+++ src/app/senha/page.js
@@ -25,7 +25,7 @@ export default function PasswordPage() {
     const cpf = localStorage.getItem('userCpf');
 
     try {
-      await axios.post('http://localhost:8000/api/users/senha', {
+      await axios.post('/api/users/senha', {
         cpf,
         passwordNumber,
       });
