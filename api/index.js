1d7975390ddcd8a8601fc28dbda65b0e0638685b
KarolineKS <kkohlsstangherlin@gmail.com>
2024-06-24 21:20:37 -0300

    :rocket: [feat]: Arrumando index da api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
+// index.js (api/index.js)
+
 import express from 'express';
 import userRouter from './routes/user.js';
 import cors from 'cors';
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use('/api/users', userRouter);
+app.use(express.json()); // Middleware para processar corpos JSON das requisições
+app.use(cors()); // Middleware para permitir requisições CORS
+app.use('/api/users', userRouter); // Roteamento para /api/users
 
-export default app;
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Servidor API rodando na porta ${PORT}`);
+});
