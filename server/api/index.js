2c5dcb40c60bfdf4aac9887fac2a731ab955e836
KarolineKS <kkohlsstangherlin@gmail.com>
2024-06-25 10:00:51 -0300

    arrumando rotas

--- server/api/index.js
+++ server/api/index.js
@@ -6,9 +6,15 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/users', usersRouter);
+app.get('/api', (req, res) => {
+  res.json({ok: true});
+}
+);
 app.use(cors());
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+export default app;
